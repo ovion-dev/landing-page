f70dd0d9cf1855cfd7720d311e1ead040373bfa2
Matteo Murgolo <matteo.murgolo@epitech.eu>
2024-11-20 23:47:40 +0100

    ADD: Next.js config for deploy on GH Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/landing-page',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
